a44beee8b38c89d4e84bf8f1c34613014fbec2cc
Nathan Keim <nkeim@seas.upenn.edu>
2013-07-26 22:04:42 -0400

    Added rudimentary nose tests of track2disk and read module

--- bigtracks/tests.py
+++ bigtracks/tests.py
@@ -1,8 +1,11 @@
-import numpy as np
+import os.path, tempfile, shutil
+from glob import glob
 import random
+import numpy as np
+import scipy.misc
 from trackpy import identification
 
-from . import track
+from . import track, read
 
 def fake_image(motion_seed=1, pos_seed=314, size=200, maxdisp=3):
     """Generate a fake frame of "particles" spaced at least 20 px apart,
@@ -30,3 +33,31 @@ def test_identification():
     params = dict(featsize=4, bphigh=1, threshold=0.3)
     ftr = track.identify_frame((img.max() - img) / img.max(), params)
     assert np.max(np.abs(ftr.y - np.array(sorted(y)))) < 0.1
+
+class test_pipeline():
+    # i.e. track2disk
+    def setUp(self):
+        self.testdir = tempfile.mkdtemp()
+        self.extension = 'PNG'
+        self.outputfile = os.path.join(self.testdir, 'bttest_tracks.h5')
+        self.nframes = 10
+        for framenumber in range(self.nframes):
+            x, y, img = fake_image(framenumber)
+            self.nparticles = len(x)
+            scipy.misc.imsave(os.path.join(self.testdir, 
+                'bttest_%.4i.%s' % (framenumber, self.extension)), img)
+    def test_tracking(self):
+        imgfiles = glob(os.path.join(self.testdir, '*.' + self.extension))
+        params = dict(bright=1, featsize=5, bphigh=2, threshold=0.5, maxdisp=3 * np.sqrt(8))
+        track.track2disk(imgfiles, self.outputfile, params)
+        bt = read.BigTracks(self.outputfile)
+        assert bt.maxframe() == self.nframes
+        assert len(bt.get_all()) == self.nframes * self.nparticles
+        assert bt[1].frame.values[0] == 1.0
+        btq = read.compute_quality(bt, frame_interval=1)
+        assert btq.Nconserved.values[-1] == self.nparticles
+    def tearDown(self):
+        shutil.rmtree(self.testdir)
+
+
+
